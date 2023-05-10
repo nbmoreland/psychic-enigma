5d3fe3342e56e3d4675f111e4a4367e66fc64811
nicholasmoreland <nmoreland18@outlook.com>
2023-05-09 22:34:58 -0500

    commit: navbar overhaul :)

--- app/nav/navbar.tsx
+++ app/nav/navbar.tsx
@@ -1,7 +1,145 @@
+import Link from "next/link";
+import { GoMarkGithub } from "react-icons/go";
+
 export default function NavBar() {
-    return (
-        <nav>
-            
-        </nav>
-    );
+  return (
+    <div className="sticky top-0 z-40 w-full backdrop-blur flex-none transition-colors duration-500 lg:z-50 lg:border-b lg:border-slate-900/10 dark:border-slate-50/[0.06] bg-white/95 supports-backdrop-blur:bg-white/60 dark:bg-transparent">
+      <div className="max-w-8xl mx-auto">
+        <div className="py-4 border-b border-slate-900/10 lg:px-8 lg:border-0 dark:border-slate-300/10 mx-4 lg:mx-0">
+          <div className="relative flex items-center">
+            <div className="relative hidden lg:flex items-center ml-auto">
+              <nav className="text-sm leading-6 font-semibold text-slate-700 dark:text-slate-200">
+                <ul className="flex space-x-8">
+                  <li>
+                    <Link
+                      href="/"
+                      className="hover:text-sky-500 dark:hover:text-sky-400"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/resume"
+                      className="hover:text-sky-500 dark:hover:text-sky-400"
+                    >
+                      Resume
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/projects"
+                      className="hover:text-sky-500 dark:hover:text-sky-400"
+                    >
+                      Projects
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/about"
+                      className="hover:text-sky-500 dark:hover:text-sky-400"
+                    >
+                      About
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
+              <div className="flex items-center border-l border-slate-200 ml-6 pl-6 dark:border-slate-800">
+                <label
+                  className="sr-only"
+                  id="headlessui-listbox-label-:r2:"
+                  data-headlessui-state=""
+                >
+                  Theme
+                </label>
+                <button
+                  type="button"
+                  id="headlessui-listbox-button-:r3:"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  data-headlessui-state=""
+                  aria-labelledby="headlessui-listbox-label-:r2: headlessui-listbox-button-:r3:"
+                >
+                  <span className="dark:hidden">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                        className="fill-sky-400/20 stroke-sky-500"
+                      ></path>
+                      <path
+                        d="M12 4v1M17.66 6.344l-.828.828M20.005 12.004h-1M17.66 17.664l-.828-.828M12 20.01V19M6.34 17.664l.835-.836M3.995 12.004h1.01M6 6l.835.836"
+                        className="stroke-sky-500"
+                      ></path>
+                    </svg>
+                  </span>
+                  <span className="hidden dark:inline">
+                    <svg viewBox="0 0 24 24" fill="none" className="w-6 h-6">
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M17.715 15.15A6.5 6.5 0 0 1 9 6.035C6.106 6.922 4 9.645 4 12.867c0 3.94 3.153 7.136 7.042 7.136 3.101 0 5.734-2.032 6.673-4.853Z"
+                        className="fill-sky-400/20"
+                      ></path>
+                      <path
+                        d="m17.715 15.15.95.316a1 1 0 0 0-1.445-1.185l.495.869ZM9 6.035l.846.534a1 1 0 0 0-1.14-1.49L9 6.035Zm8.221 8.246a5.47 5.47 0 0 1-2.72.718v2a7.47 7.47 0 0 0 3.71-.98l-.99-1.738Zm-2.72.718A5.5 5.5 0 0 1 9 9.5H7a7.5 7.5 0 0 0 7.5 7.5v-2ZM9 9.5c0-1.079.31-2.082.845-2.93L8.153 5.5A7.47 7.47 0 0 0 7 9.5h2Zm-4 3.368C5 10.089 6.815 7.75 9.292 6.99L8.706 5.08C5.397 6.094 3 9.201 3 12.867h2Zm6.042 6.136C7.718 19.003 5 16.268 5 12.867H3c0 4.48 3.588 8.136 8.042 8.136v-2Zm5.725-4.17c-.81 2.433-3.074 4.17-5.725 4.17v2c3.552 0 6.553-2.327 7.622-5.537l-1.897-.632Z"
+                        className="fill-sky-500"
+                      ></path>
+                      <path
+                        fill-rule="evenodd"
+                        clip-rule="evenodd"
+                        d="M17 3a1 1 0 0 1 1 1 2 2 0 0 0 2 2 1 1 0 1 1 0 2 2 2 0 0 0-2 2 1 1 0 1 1-2 0 2 2 0 0 0-2-2 1 1 0 1 1 0-2 2 2 0 0 0 2-2 1 1 0 0 1 1-1Z"
+                        className="fill-sky-500"
+                      ></path>
+                    </svg>
+                  </span>
+                </button>
+
+                <a
+                  href="https://github.com/nicholasmoreland"
+                  className="ml-6 block text-slate-400 hover:text-slate-500 dark:hover:text-slate-300"
+                >
+                  <span className="sr-only">Nicholas Moreland on GitHub</span>
+                  <svg
+                    viewBox="0 0 16 16"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                  </svg>
+                </a>
+
+                <a
+                  href="https://www.linkedin.com/in/nicholas-moreland-cse/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-6 block text-blue-600 hover:text-blue-700 dark:text-blue-400 dark:hover:text-blue-300"
+                >
+                  <span className="sr-only">LinkedIn Profile</span>
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M22.516 2H1.485C0.65 2 0 2.651 0 3.485v17.03C0 21.349.65 22 1.485 22h21.03c.835 0 1.485-.651 1.485-1.485V3.485c0-.834-.65-1.485-1.485-1.485zM7.908 18.256h-3.07V9.062h3.07v9.194zM6.37 8.114h-.022c-.976 0-1.611-.678-1.611-1.529 0-.865.651-1.529 1.647-1.529.996 0 1.612.664 1.634 1.529.011.851-.612 1.529-1.648 1.529zM19.2 18.256h-3.076v-4.803c0-1.146-.41-1.929-1.435-1.929-.777 0-1.239.522-1.442 1.024-.074.18-.09.432-.09.686v4.022h-3.07s.04-8.354 0-9.194h3.07v1.301c.408-.623 1.132-1.506 2.753-1.506 2.018 0 3.532 1.318 3.532 4.175v5.224z"
+                    ></path>
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -16,16 +16,6 @@ export default function Home() {
 
       <main className='bg-white px-10 md:px-20 lg:px-40'>
         <section className='min-h-screen'>
-          <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Software Engineer</h1>
-            <ul className='flex items-center'>
-              <li>
-                <BsFillMoonStarsFill className='cursor-point text-2xl'/>
-              </li>
-              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-sm ml-8' href='#'>Resume</a></li>
-            </ul>
-          </nav>
-
           <div className='text-center'>
             <h2 className='text-4xl py-2 text-teal-600 font-medium md:text-6xl'>Nicholas Moreland</h2>
             <h3 className='text-2xl py-2 md:text-3xl'>Developer and designer.</h3>
