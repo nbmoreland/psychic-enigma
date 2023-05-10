e4d5eea4ec9515d7b8be687033e3ce4a2c55aeb6
nicholasmoreland <nmoreland18@outlook.com>
2023-05-10 16:25:02 -0500

    update: working on project cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import design from "../public/design.png";
+import cherry from "../public/pattern-of-fresh-cherries-on-pink-background-GEMF03873.jpg";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
 
@@ -18,18 +18,18 @@ export default function Home() {
 
         <section className="min-h-screen">
           <div className="lg:flex gap-10 mx-4 my-4">
-            <div className="dark:bg-white text-center shadow-lg p-10 rounded-xl my-10 flex flex-col justify-center items-center">
-              <Image src={design} width={100} height={100} alt={""} />
-              <h3 className="text-lg font-medium pt-8 pb-2">
-                Beatiful Designs
-              </h3>
-              <p className="py-2">
-                Creating elegant designs suited from your needs.
-              </p>
-              <h4 className="text-teal-600 py-4">Design tools I use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
+            <div className="bg-white text-center shadow-lg rounded-xl flex flex-col">
+              <div className="border-slate-900/10 dark:border-slate-300/10 w-96 align-top mb-10">
+                <Image
+                  src={cherry}
+                  width={400}
+                  height={200}
+                  alt={""}
+                  className="rounded-tr-xl rounded-tl-xl"
+                />
+
+                <h3 className="text-lg font-medium pt-1">Project Cherry</h3>
+              </div>
             </div>
 
             <div className="dark:bg-white text-center shadow-lg p-10 rounded-xl my-10 flex flex-col justify-center items-center">
