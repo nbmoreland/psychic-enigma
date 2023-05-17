0e422758a82f0ee9b5e99a1ecd969079a3c98056
nicholasmoreland <nmoreland18@outlook.com>
2023-05-17 01:59:46 -0500

    update: padding for menu button

--- app/nav/navbar.tsx
+++ app/nav/navbar.tsx
@@ -38,7 +38,7 @@ const NavBar = ({ dark }: { dark: boolean }) => {
           <div className="block lg:hidden relative items-center mr-2 ">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+              className="font-medium px-2 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
             >
               {isOpen ? (
                 <IoCloseOutline className="fill-current h-6 w-6" />
