69186aa16573bf161dbf2f1fb5bf28543e6621e3
nicholasmoreland <nmoreland18@outlook.com>
2023-05-17 02:04:48 -0500

    update: final fix

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,20 +1,17 @@
 "use client";
 
 import { NavBar } from "../nav/navbar";
-import { useState } from "react";
 
-export default function AboutPage({ dark }: { dark: boolean }) {
+export default function AboutPage() {
   return (
-    <div className={dark ? "dark" : ""}>
-      <main className="bg-white dark:bg-gray-900">
-        <NavBar dark={dark} />
+    <main className="bg-white dark:bg-gray-900">
+      <NavBar />
 
-        <section className="min-h-screen">
-          <div className="lg:flex gap-10 mx-4 my-4">
-            <h1>Welcome to the about page.</h1>
-          </div>
-        </section>
-      </main>
-    </div>
+      <section className="min-h-screen">
+        <div className="lg:flex gap-10 mx-4 my-4">
+          <h1>Welcome to the about page.</h1>
+        </div>
+      </section>
+    </main>
   );
 }
