4f6b00611bffd5537df0a42d08286833e3d9eedf
nicholasmoreland <nmoreland18@outlook.com>
2023-05-18 19:30:56 -0500

    update: started about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,14 +4,45 @@ import { NavBar } from "../nav/navbar";
 
 export default function AboutPage() {
   return (
-    <main className="bg-white dark:bg-gray-900">
+    <main className="bg-white dark:bg-gray-900 min-h-screen">
       <NavBar />
 
-      <section className="min-h-screen">
-        <div className="lg:flex gap-10 mx-4 my-4">
-          <h1>Welcome to the about page.</h1>
+      <div className="mx-auto min-w-screen pt-12 pb-2 px-2 lg:py-16 lg:px-8">
+        <div className="lg:text-center">
+          <h2 className="text-base text-indigo-600 dark:text-indigo-400 font-semibold tracking-wide uppercase">
+            About
+          </h2>
+          <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            Where are you from?
+          </p>
+          <p className="mt-2 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            I was born and raised in Dallas, TX. My birthday is July 23, 1999.
+          </p>
+
+          <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            Where do you go to school?
+          </p>
+          <p className="mt-2 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            The University of Texas at Arlington.
+          </p>
+
+          <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            What are you studying?
+          </p>
+          <p className="mt-2 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Bachelor of Science in Computer Science BSCS.
+          </p>
+
+          <p className="mt-2 text-3xl leading-8 font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            What do you plan for after College?
+          </p>
+          <p className="mt-2 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            I plan to go into the workforce as a software engineer while
+            pursuing a Online Master of Science in Computer Science with
+            Artificial Intelligence Specialization.
+          </p>
         </div>
-      </section>
+      </div>
     </main>
   );
 }
